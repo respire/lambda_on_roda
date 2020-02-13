c9cffff007b60a4bef887881aa629024d88d6375
Respire <wingcat@live.com>
2020-02-14 02:09:09 +0800

    support roda

--- hello_world/app.rb
+++ hello_world/app.rb
@@ -2,8 +2,108 @@
 
 require 'httparty'
 require 'json'
+require 'roda'
+
+class App < Roda
+  plugin :json
+
+  route do |r|
+    r.root do
+      'Amazon lambda on ruby'
+    end
+
+    r.get('hello') do
+      res = HTTParty.get('http://checkip.amazonaws.com/')
+      {
+        message: 'Hello World!',
+        code: res.code,
+        location: res.body
+      }
+    end
+  end
+end
+
+RackHandler = App.freeze.app
+SKIP_HEADERS = %w[Content-Type Content-Length rack.hijack].freeze
+
+def lambda_to_rack(event)
+  # we trust amazon's input
+  headers = event['headers']
+
+  env = {}
+  env['REQUEST_METHOD'] = event['httpMethod']
+
+  # it hurts me :(
+  env['QUERY_STRING'] = event['queryStringParameters'].to_a.map! { |pair| pair.map(&:to_s).join('=') }.join('&')
+
+  env['SERVER_NAME'] = headers['Host']
+  env['REMOTE_HOST'] = headers['Host']
+  env['REMOTE_ADDR'] = headers['X-Forwarded-For']
+  env['SERVER_PORT'] = headers['X-Forwarded-Port']
+  env['rack.url_scheme'] = headers['X-Forwarded-Proto']
+
+  # always HTTP 1.1? :(
+  env['HTTP_VERSION'] = 'HTTP/1.1'
+
+  env['REQUEST_PATH'] = event['path']
+  env['PATH_INFO'] = event['path']
+  env['SCRIPT_NAME'] = ''
+  env['REQUEST_URI'] = "#{headers['X-Forwarded-Proto']}://#{headers['Host']}#{event['path']}"
+  env['REQUEST_URI'] += "?#{env['QUERY_STRING']}" unless env['QUERY_STRING'].empty?
+
+  headers.each do |k, v|
+    next if SKIP_HEADERS.include?(k)
+
+    env["HTTP_#{k.upcase.tr('-', '_')}"] = v
+  end
+
+  input = StringIO.new
+  input.set_encoding(Encoding::ASCII_8BIT)
+  input << event['body']
+  input.rewind
+
+  env['rack.input'] = input
+  env['rack.errors'] = STDERR
+  env['rack.hijack?'] = false
+  env['rack.version'] = Rack::VERSION
+
+  env
+end
+
+def rack_to_lambda(status, headers, body)
+  res = {
+    'isBase64Encoded' => false,
+    'statusCode' => status,
+    'multiValueHeaders' => {}
+  }
+
+  headers.each do |k, v|
+    next if SKIP_HEADERS.include?(k)
+
+    hv = v.split("\n")
+    res['multiValueHeaders'][k] = hv
+  end
+
+  if body.respond_to?(:to_path)
+    path = body.to_path
+    # not support stream. so, good luck.
+    res['body'] = File.binread(path)
+  else
+    res['body'] = body.join
+  end
+
+  body.close if body.respond_to?(:close)
+
+  res
+end
 
 def lambda_handler(event:, context:)
+  env = lambda_to_rack(event)
+  status, headers, body = RackHandler.call(env)
+  rack_to_lambda(status, headers, body)
+end
+
+# def lambda_handler(event:, context:)
   # Sample pure Lambda function
 
   # Parameters
@@ -23,15 +123,15 @@ def lambda_handler(event:, context:)
   #     # api-gateway-simple-proxy-for-lambda-output-format
   #     Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
 
-  response = HTTParty.get('http://checkip.amazonaws.com/')
-  {
-    statusCode: response.code,
-    body: {
-      message: 'Hello World!',
-      location: response.body
-    }.to_json
-  }
-rescue HTTParty::Error => e
-  puts e.inspect
-  raise e
-end
+#   response = HTTParty.get('http://checkip.amazonaws.com/')
+#   {
+#     statusCode: response.code,
+#     body: {
+#       message: 'Hello World!',
+#       location: response.body
+#     }.to_json
+#   }
+# rescue HTTParty::Error => e
+#   puts e.inspect
+#   raise e
+# end
--- tests/unit/test_handler.rb
+++ tests/unit/test_handler.rb
@@ -7,23 +7,23 @@ require 'mocha/test_unit'
 require_relative '../../hello_world/app'
 
 class HelloWorldTest < Test::Unit::TestCase
-  def event
+  def event_on_root
     {
-      body: 'eyJ0ZXN0IjoiYm9keSJ9',
-      resource: '/{proxy+}',
-      path: '/path/to/resource',
-      httpMethod: 'POST',
-      isBase64Encoded: true,
-      queryStringParameters: {
-        foo: 'bar'
+      'body' => 'eyJ0ZXN0IjoiYm9keSJ9',
+      'resource' => '/',
+      'path' => '/',
+      'httpMethod' => 'GET',
+      'isBase64Encoded' => true,
+      'queryStringParameters' => {
+        'foo' => 'bar'
       },
-      pathParameters: {
-        proxy: '/path/to/resource'
+      'pathParameters' => {
+        'proxy' => '/path/to/resource'
       },
-      stageVariables: {
-        baz: 'qux'
+      'stageVariables' => {
+        'baz' => 'qux'
       },
-      headers: {
+      'headers' => {
         'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
         'Accept-Encoding' => 'gzip, deflate, sdch',
         'Accept-Language' => 'en-US,en;q=0.8',
@@ -43,31 +43,96 @@ class HelloWorldTest < Test::Unit::TestCase
         'X-Forwarded-Port' => '443',
         'X-Forwarded-Proto' => 'https'
       },
-      requestContext: {
-        accountId: '123456789012',
-        resourceId: '123456',
-        stage: 'prod',
-        requestId: 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
-        requestTime: '09/Apr/2015:12:34:56 +0000',
-        requestTimeEpoch: 1_428_582_896_000,
-        identity: {
-          cognitoIdentityPoolId: 'null',
-          accountId: 'null',
-          cognitoIdentityId: 'null',
-          caller: 'null',
-          accessKey: 'null',
-          sourceIp: '127.0.0.1',
-          cognitoAuthenticationType: 'null',
-          cognitoAuthenticationProvider: 'null',
-          userArn: 'null',
-          userAgent: 'Custom User Agent String',
-          user: 'null'
+      'requestContext' => {
+        'accountId' => '123456789012',
+        'resourceId' => '123456',
+        'stage' => 'prod',
+        'requestId' => 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
+        'requestTime' => '09/Apr/2015:12:34:56 +0000',
+        'requestTimeEpoch' => 1_428_582_896_000,
+        'identity' => {
+          'cognitoIdentityPoolId' => 'null',
+          'accountId' => 'null',
+          'cognitoIdentityId' => 'null',
+          'caller' => 'null',
+          'accessKey' => 'null',
+          'sourceIp' => '127.0.0.1',
+          'cognitoAuthenticationType' => 'null',
+          'cognitoAuthenticationProvider' => 'null',
+          'userArn' => 'null',
+          'userAgent' => 'Custom User Agent String',
+          'user' => 'null'
         },
-        path: '/prod/path/to/resource',
-        resourcePath: '/{proxy+}',
-        httpMethod: 'POST',
-        apiId: '1234567890',
-        protocol: 'HTTP/1.1'
+        'path' => '/prod/path/to/resource',
+        'resourcePath' => '/{proxy+}',
+        'httpMethod' => 'POST',
+        'apiId' => '1234567890',
+        'protocol' => 'HTTP/1.1'
+      }
+    }
+  end
+
+  def event_on_hello
+    {
+      'body' => 'eyJ0ZXN0IjoiYm9keSJ9',
+      'resource' => '/{proxy+}',
+      'path' => '/hello',
+      'httpMethod' => 'GET',
+      'isBase64Encoded' => true,
+      'queryStringParameters' => {
+        'foo' => 'bar'
+      },
+      'pathParameters' => {
+        'proxy' => '/path/to/resource'
+      },
+      'stageVariables' => {
+        'baz' => 'qux'
+      },
+      'headers' => {
+        'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Encoding' => 'gzip, deflate, sdch',
+        'Accept-Language' => 'en-US,en;q=0.8',
+        'Cache-Control' => 'max-age=0',
+        'CloudFront-Forwarded-Proto' => 'https',
+        'CloudFront-Is-Desktop-Viewer' => 'true',
+        'CloudFront-Is-Mobile-Viewer' => 'false',
+        'CloudFront-Is-SmartTV-Viewer' => 'false',
+        'CloudFront-Is-Tablet-Viewer' => 'false',
+        'CloudFront-Viewer-Country' => 'US',
+        'Host' => '1234567890.execute-api.us-east-1.amazonaws.com',
+        'Upgrade-Insecure-Requests' => '1',
+        'User-Agent' => 'Custom User Agent String',
+        'Via' => '1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)',
+        'X-Amz-Cf-Id' => 'cDehVQoZnx43VYQb9j2-nvCh-9z396Uhbp027Y2JvkCPNLmGJHqlaA==',
+        'X-Forwarded-For' => '127.0.0.1, 127.0.0.2',
+        'X-Forwarded-Port' => '443',
+        'X-Forwarded-Proto' => 'https'
+      },
+      'requestContext' => {
+        'accountId' => '123456789012',
+        'resourceId' => '123456',
+        'stage' => 'prod',
+        'requestId' => 'c6af9ac6-7b61-11e6-9a41-93e8deadbeef',
+        'requestTime' => '09/Apr/2015:12:34:56 +0000',
+        'requestTimeEpoch' => 1_428_582_896_000,
+        'identity' => {
+          'cognitoIdentityPoolId' => 'null',
+          'accountId' => 'null',
+          'cognitoIdentityId' => 'null',
+          'caller' => 'null',
+          'accessKey' => 'null',
+          'sourceIp' => '127.0.0.1',
+          'cognitoAuthenticationType' => 'null',
+          'cognitoAuthenticationProvider' => 'null',
+          'userArn' => 'null',
+          'userAgent' => 'Custom User Agent String',
+          'user' => 'null'
+        },
+        'path' => '/prod/path/to/resource',
+        'resourcePath' => '/{proxy+}',
+        'httpMethod' => 'POST',
+        'apiId' => '1234567890',
+        'protocol' => 'HTTP/1.1'
       }
     }
   end
@@ -79,18 +144,34 @@ class HelloWorldTest < Test::Unit::TestCase
     end
   end
 
-  def expected_result
+  def expected_result_from_hello
     {
-      statusCode: 200,
-      body: {
-        message: 'Hello World!',
-        location: '1.1.1.1'
+      'statusCode' => 200,
+      'isBase64Encoded' => false,
+      'multiValueHeaders' => {},
+      'body' => {
+        'message' => 'Hello World!',
+        'code' => 200,
+        'location' => '1.1.1.1'
       }.to_json
     }
   end
 
-  def test_lambda_handler
+  def expected_result_from_root
+    {
+      'statusCode' => 200,
+      'isBase64Encoded' => false,
+      'multiValueHeaders' => {},
+      'body' => 'Amazon lambda on ruby'
+    }
+  end
+
+  def test_lambda_handler_on_hello
     HTTParty.expects(:get).with('http://checkip.amazonaws.com/').returns(mock_response)
-    assert_equal(lambda_handler(event: event, context: ''), expected_result)
+    assert_equal(lambda_handler(event: event_on_hello, context: ''), expected_result_from_hello)
+  end
+
+  def test_lambda_handler_on_root
+    assert_equal(lambda_handler(event: event_on_root, context: ''), expected_result_from_root)
   end
 end
